a9ad758f8b07691421e94d4ba16cba1511aaa3fe
Fei Zhang <fzhcis@rit.edu>
2025-09-08 16:31:45 -0400

    fix: update dataset path for Semantic3D in run_randlanet_semantic3d.py

--- run_randlanet_semantic3d.py
+++ run_randlanet_semantic3d.py
@@ -7,7 +7,7 @@ cfg = _ml3d.utils.Config.load_from_file(cfg_file)
 
 model = ml3d.models.RandLANet(**cfg.model)
 # cfg.dataset['dataset_path'] = "/home/fzhcis/data/semantic3d_full/preprocessed/Semantic3D"
-cfg.dataset['dataset_path'] = "/home/fzhcis/data/semantic3d_full/Semantic3D"
+cfg.dataset['dataset_path'] = "/shared/rc/mangrove/data/Semantic3D/"
 
 dataset = ml3d.datasets.Semantic3D(cfg.dataset.pop('dataset_path', None), **cfg.dataset)
 pipeline = ml3d.pipelines.SemanticSegmentation(model, dataset=dataset, device="gpu", **cfg.pipeline)
